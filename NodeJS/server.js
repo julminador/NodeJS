f4b5679c9d22e589754c14e799619008da227a44
Julminador <juliansosa021@gmail.com>
2021-09-16 09:40:14 -0500

    Manejo de query Http con body-parser

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -2,22 +2,38 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //import express from 'express'; //Sintaxis ES6
 const router = express.Router();
+const response = require('./network/response');
 
 var app = express();
+app.use(bodyParser.urlencoded({"extended": false}));
 app.use(bodyParser.json());
 app.use(router);
 
 router.get('/message', (req, res) => {
-    res.send('Lista de mensajes');
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Nuestro valor personalizado",
+    });
+    // res.send('Lista de mensajes');
+    response.success(req, res, 'Lista de mensajes');
 });
 
 router.post('/message', (req, res) => {
-    res.send('Mensaje añadido');
+    //res.send('Mensaje añadido');
+    console.log(req.body);
+    if (req.query.error == 'ok') {
+        response.error(req, res, 'Error simulado', 400);
+    } else {
+        response.success(req, res, 'Creado correctamente', 201);
+    }
+    // res.status(201).send({error: '', body: 'Creado correctamente'});
 });
 
 router.delete('/message', function (req, res) {
+    // console.log(req.query);
     console.log(req.body);
-    res.send('Mensaje añadido correctamente');
+    res.send('Mensaje '+ req.body.text +' eliminado correctamente');
+
 });
 
 // app.use('/', (req, res) => {
