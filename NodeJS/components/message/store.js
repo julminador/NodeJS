d7402e8eb4c5f645a631c807836f7499a48c805b
julminador <juliansosa021@gmail.com>
2021-09-20 08:17:03 -0500

    store.js

--- NodeJS/components/message/store.js
+++ NodeJS/components/message/store.js
@@ -1,3 +1,38 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const {config} = require('../../config');
+
+const USER = encodeURIComponent(config.DB_USER);
+const PASSWORD = encodeURIComponent(config.DB_PASSWORD);
+const DB_NAME = encodeURIComponent(config.DB_NAME);
+
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.DB_HOST}:${config.port}/${DB_NAME}?retryWrites=true&w=majority`;
+
+class MongoLib {
+    constructor(){
+        this.client = new MongoClient(MONGO_URI, {useNewUrlParser: true });
+        this.db_name = DB_NAME;
+    }
+    connect(){
+        if(MongoLib.connection){
+            MongoLib.connection = new Promise((resolve, reject) => {
+                this.client.connect( err => {
+                    if(err){
+                        reject(err);
+                    }
+                    console.log('conectado');
+                    resolve(this.client.db(this.db_name));    
+                });
+            });
+        }
+        return MongoLib.connection;
+    }
+}
+
+module.exports = MongoLib;
+
+
+
+
 const { get } = require("./network");
 
 const list = [];
