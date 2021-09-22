38879f4559f3ecbb9b37306e4860ebec2cf83101
julminador <juliansosa021@gmail.com>
2021-09-22 12:31:56 -0500

    Integración de WebSocket a la API

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 //import express from 'express'; //Sintaxis ES6
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const port = 3000;
 
+const bodyParser = require('body-parser');
+const socket = require('./socket');
 const db = require('./db');
 
 const router = require('./network/routes');
 
 db('mongodb+srv://db_user_platzinodejs:rNhUxhpuBrjkGYja@cluster0.shxg8.mongodb.net/platzinodejs_db');
 
-var app = express();
 app.use(bodyParser.urlencoded({"extended": false}));
 app.use(bodyParser.json());
+
+socket.connect(server);
 
 router(app);
 
 //Servir archivos estaticos
 app.use('/app', express.static('public'));
 
-let port = 3000
-app.listen(port);
-console.log(`La aplicación está escuchando en http://localhost:${port}`);
+server.listen(port, () => {
+    console.log(`La aplicación está escuchando en http://localhost:${port}`);
+});
