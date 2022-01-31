d3bed2de1cec5b1d95a50d0cc5de66757ae7fcfc
julminador <juliansosa021@gmail.com>
2022-01-31 18:28:25 -0500

    Fix host url because it doesn't have a security certificate

--- nodeJS/config.js
+++ nodeJS/config.js
@@ -1,6 +1,6 @@
 const config = {
     dbUrl: process.env.DB_URL || 'mongodb+srv://db_user_platzinodejs:rNhUxhpuBrjkGYja@cluster0.shxg8.mongodb.net/platzinodejs_db',
-    host: process.env.HOST || 'https://localhost',
+    host: process.env.HOST || 'http://localhost',
     port: process.env.PORT || 3000,
     publicRoute: process.env.PUBLIC_ROUTE || '/app',
 };
